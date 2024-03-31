00dfb2b6cf84c9622ed016546c94681bd1733c56
pablosky <pablocacaster@gmail.com>
2024-03-31 10:02:18 +0200

    Update check_backdoor_xz.sh

--- check_backdoor_xz.sh
+++ check_backdoor_xz.sh
@@ -5,9 +5,8 @@ if [ $? -eq 0 ]; then
   xz --version | grep -E "5.6.0|5.6.1"
   # if version is  5.6.0 or  5.6.1, then it is vulnerable 
   if [ $? -eq 0 ]; then
-    echo "xz is vulnerable"
+    echo "xz is vulnerable upgrade your version usign brew update"
     # we downgrade 5.4.6
-    apt-get install xz-utils=5.4.6-0ubuntu1
   else
     echo "xz is not vulnerable"
   fi
